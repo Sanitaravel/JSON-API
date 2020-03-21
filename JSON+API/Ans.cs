7d797c68496d4f55f2dfb33b2c42476ff48ecbea
Alexvue <58359823+Alexvue@users.noreply.github.com>
2020-03-21 16:42:29 +0300

    End of lesson

---
+++ JSON+API/Ans.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_API
+{
+    public class Ans
+    {
+        public Class_Coord Coord { get; set; }
+        public List<Class_Weather> Weather { get; set; }
+        public string Base { get; set; }
+        public Class_Main main { get; set; }
+        public int Visibility { get; set; }
+        public Class_Wind Wind { get; set; }
+        public Class_Clouds Clouds { get; set; }
+        public int dt { get; set; }
+        public Class_Sys Sys { get; set; }
+        public int Timezone { get; set; }
+        public int Id { get; set; }
+        public string name { get; set; }
+        public int Cod { get; set; }
+    }
+
+    public class Class_Coord
+    {
+        public double Lon;
+        public double Lan;
+    }
+
+    public class Class_Weather
+    {
+        public int Id;
+        public string Main;
+        public string Description;
+        public string Icon;
+    }
+
+    public class Class_Main
+    {
+        public double Temp;
+        public double Feels_like;
+        public double Temp_min;
+        public double Temp_max;
+        public int Pressure;
+        public int Humidity;
+    }
+
+    public class Class_Wind
+    {
+        public int Speed;
+        public int Deg;
+    }
+
+    public class Class_Clouds
+    {
+        public int all;
+    }
+
+    public class Class_Sys
+    {
+        public int Type;
+        public int Id;
+        public string Country;
+        public long Sunrise;
+        public long Sunset;
+    }
+}
--- JSON+API/MainWindow.xaml.cs
+++ JSON+API/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace JSON_API
 {
@@ -20,9 +23,41 @@ namespace JSON_API
     /// </summary>
     public partial class MainWindow : Window
     {
+        string key = "3b5ae7f192eee2751d4c10d69cf0012c";
+        string town = "";
+        string url = "http://api.openweathermap.org/data/2.5/weather?q=";
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void getInf()
+        {
+            string url_tmp = url + town + "&units=metric&appid=" + key + "&lang=ru";
+            var webRequest = WebRequest.Create(url_tmp);
+            var responseStream = webRequest.GetResponse().GetResponseStream();
+            string answer = "";
+            if (responseStream != null)
+            {
+                using (var streamReader = new StreamReader(responseStream))
+                {
+                    answer = streamReader.ReadLine();
+                }
+                Ans ans = JsonConvert.DeserializeObject<Ans>(answer);
+                to_tb(ans);
+            }
+        }
+
+        private void Bt_know_Click(object sender, RoutedEventArgs e)
+        {
+            town = tb_city.Text;
+            getInf();
+        }
+
+        private void to_tb(Ans ans)
+        {
+            tb_information.Text = "В городе " + town + " следующая погода:\n\n";
+            tb_information.Text += "Температура: " + ans.main.Temp.ToString() + "°";
         }
     }
 }
